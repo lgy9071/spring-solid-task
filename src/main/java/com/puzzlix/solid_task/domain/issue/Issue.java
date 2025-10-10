4e803261c29f2fc5e1974620d0447b1180f870d9
lgy9071 <lgy990711@naver.com>
2025-10-10 10:45:33 +0900

    유저, 이슈, 프로젝트 도메인 객체 설계

---
+++ src/main/java/com/puzzlix/solid_task/domain/issue/Issue.java
@@ -0,0 +1,26 @@
+package com.puzzlix.solid_task.domain.issue;
+// pk
+// 타이틀
+// 내용
+// 진행상태
+
+// 프로젝트 pk
+// 보고자 (누가 요청 했는지)
+// 담당자 (누가 처리 하는지)
+
+public class Issue {
+
+    private Long id;
+    private String name;
+    private String description;
+    private IssueStatus issueStatus;
+
+    // 추후 연관관계 필드
+    private Long projectId;
+
+    //누가 요청(보고)
+    private Long reporterId;
+
+    // 담당자(누군가에게 할당 되어 처리 됩니다)
+    private Long assigneeId;
+}
