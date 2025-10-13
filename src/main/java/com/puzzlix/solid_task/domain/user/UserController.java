b9993061bd0ef2cfee79067d8d614fb78b38eee8
lgy9071 <lgy990711@naver.com>
2025-10-13 15:12:47 +0900

    Jwt 구축 및 로그인, 회원 구현

---
+++ src/main/java/com/puzzlix/solid_task/domain/user/UserController.java
@@ -0,0 +1,42 @@
+package com.puzzlix.solid_task.domain.user;
+
+import com.puzzlix.solid_task._global.config.jwt.JwtTokenProvider;
+import com.puzzlix.solid_task._global.dto.CommonResponseDto;
+import com.puzzlix.solid_task.domain.user.dto.UserRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/users")
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<CommonResponseDto<?>> signUp(@Valid @RequestBody UserRequest.SignUp request){
+
+        userService.signUp(request);
+        return ResponseEntity.ok(CommonResponseDto.success(null, "회원가입이 완료되었습니다."));
+    }
+
+    // 자원 요청 -> GET
+    // 자원 생성 -> POST
+    // 자원 수정 -> PUT, Patch
+    // 자원 삭제 -> DELETE
+    @PostMapping("/login")
+    public ResponseEntity<CommonResponseDto<?>> login(@Valid @RequestBody UserRequest.Login request){
+        User user = userService.login(request);
+
+        // 사용자 이메일을 기반으로 JWT 토큰 생성
+        String token = jwtTokenProvider.createToken(user.getEmail());
+        return ResponseEntity.ok(CommonResponseDto.success(token, "로그인에 성공했습니다."));
+    }
+
+}
